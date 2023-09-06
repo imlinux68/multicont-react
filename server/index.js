a913b2e3a33be0d7cc319a7f3a8600da67c68dc4
im linux <imlinux68@gmail.com>
2023-09-07 01:42:02 +0300

    added info

--- server/index.js
+++ server/index.js
@@ -6,3 +6,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log('Lost PG Connection'));
